c5e8bef3862147cca23e871b703eb7dfd7eb5a45
Kai <t3mr0i@googlemail.com>
2019-07-28 22:28:00 +0200

    removed href on card because of React/gatsby bug.

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,27 +34,27 @@ class Index extends React.Component {
       <h2>Some of my works</h2>
       <div className="CardGroup">
         <Card 
-          link="/about.js"
+        
           title="Corporate Identity"
           text="Dream Machine"
           image='../images/DreamMachine.png' />
         <Card 
-        link="https://www.behance.net/gallery/57691747/DTV-Devils-Teamlogo"
+       
           title="Logos"
           text="DTV DEVILS"
           image='/images/Devils.png' />
         <Card 
-        link="https://www.amazon.de/Kai-Detmers-Tee-Helfer/dp/B079W9RDSM"
+
           title="Smart Home"
           text="Alexa Skill Teehelfer"
           image='/images/TeeHelfer.png' />
         <Card 
-        link="http://wiki.selfhtml.org/wiki/href"
+       
           title="VR Startup"
           text="REVEL"
           image='/images/kleiner.png' />
            <Card 
-           link="http://wiki.selfhtml.org/wiki/href"
+          
           title="Biggest VR Blog in Benelux"
           text="TwistedReality"
           image='/images/Logo klein.png' />
