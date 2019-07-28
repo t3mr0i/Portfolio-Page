fff7035a448c10786c6e8120008242eb56f7e554
Kai <t3mr0i@googlemail.com>
2019-07-28 21:32:13 +0200

    Fixed Caps

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import './Card.css'
+import './card.css'
 
-const Card = props => (
-    <a href={props.link}>
-    <div className="Card">
-        <a href={props.link}/>
-        <img src={props.image} />
-        <h3>{props.title}</h3>
-        <p>{props.text}</p>
-    </div>
-    </a>
+const Card = props => ( <
+    a href = { props.link } >
+    <
+    div className = "Card" >
+    <
+    a href = { props.link }
+    /> <
+    img src = { props.image }
+    /> <
+    h3 > { props.title } < /h3> <
+    p > { props.text } < /p> <
+    /div> <
+    /a>
 )
 
 export default Card
--- src/components/header.js
+++ src/components/header.js
@@ -1,55 +1,62 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import './Header.css'
+import './header.css'
 import '../layouts/index.css'
 
 class Header extends React.Component {
 
-  
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-      hasScrolled: false
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            hasScrolled: false
+        }
     }
-  }
 
-  componentDidMount() {
-    window.addEventListener('scroll',
-    this.handleScroll)
-  }
-
-  handleScroll = (event) => {
-    const scrollTop = window.pageYOffset
-  
-    if (scrollTop > 50) {
-      this.setState({ hasScrolled: true })
-    } else {
-      this.setState({ hasScrolled: false })
+    componentDidMount() {
+        window.addEventListener('scroll',
+            this.handleScroll)
     }
-  }
 
-    
+    handleScroll = (event) => {
+        const scrollTop = window.pageYOffset
 
-  render() {
-    return (
-    <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
-    <div className={this.state.hasScrolled ? '___gatsby aScrolled' : '___gatsby'}>
+        if (scrollTop > 50) {
+            this.setState({ hasScrolled: true })
+        } else {
+            this.setState({ hasScrolled: false })
+        }
+    }
 
-  
- 
-  <div className="HeaderGroup">
-  <img width="30" src={require('../images/temroiNew.png')} />
 
-    <Link to="/courses">work</Link>
-    
-    <Link to="/workshops">me</Link>
-  </div>
-</div>
-</div>
 
-)
-}
+    render() {
+        return ( <
+            div className = { this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header' } >
+            <
+            div className = { this.state.hasScrolled ? '___gatsby aScrolled' : '___gatsby' } >
+
+
+
+            <
+            div className = "HeaderGroup" >
+            <
+            img width = "30"
+            src = { require('../images/temroiNew.png') }
+            />
+
+            <
+            Link to = "/courses" > work < /Link>
+
+            <
+            Link to = "/workshops" > me < /Link> < /
+            div > <
+            /div> < /
+            div >
+
+        )
+    }
 }
 
 export default Header
