61ea7afd20bbedb2e375eb3f402cb2d2bfed39c3
Kai <t3mr0i@googlemail.com>
2019-07-29 08:36:09 +0200

    Fixed links maybe?

--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './card.css'
+import { Link } from 'react-router-dom';
 
 const Card = props => (
-  
+    <a href={props.link}>
     <div className="Card">
-        
+       
         <img src={props.image} />
         <h3>{props.title}</h3>
         <p>{props.text}</p>
     </div>
- 
+    </a>
 )
 
 export default Card
