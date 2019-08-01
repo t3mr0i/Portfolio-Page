fe2b25e0d0cbac7d6b1af2756870db9dc3b2c73d
Kai <t3mr0i@googlemail.com>
2019-08-01 10:46:48 +0200

    Redirect instead of Link.

--- src/components/card.js
+++ src/components/card.js
@@ -3,13 +3,14 @@ import './card.css'
 import { Link } from 'react-router-dom';
 
 const Card = props => (
-    <Link to ={props.link}>
+    <Redirect to ={props.link}>
     <div className="Card">
+
         <img src={props.image} />
         <h3>{props.title}</h3>
         <p>{props.text}</p>
     </div>
-    </Link>
+    </Redirect>
 )
 
 export default Card
