164e31606012bc45d9986ad48abc641c972b194c
Kai <t3mr0i@googlemail.com>
2019-08-01 11:07:21 +0200

    Route tryout

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './card.css'
-import { Link, Redirect } from 'react-router-dom';
+import { Link, Redirect, Route } from 'react-router-dom';
 
 const Card = props => (
-    <Link to ={props.link}>
+    <Route to ={props.link}>
     <div className="Card">
 
         <img src={props.image} />
         <h3>{props.title}</h3>
         <p>{props.text}</p>
     </div>
-    </Link>
+    </Route>
 )
 
 export default Card
